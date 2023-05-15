04ae4f537770644b69fa9c68dd4d51c8f98ada21
tan-dong-yu-0 <dytan0709@gmail.com>
2023-05-16 03:35:46 +0800

    added home page route

--- Q2/client/src/router/index.js
+++ Q2/client/src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Login from "../views/Login.vue"
+import Home from "../views/Home.vue"
 
-const routes = []
+const routes = [
+	{ path: "/login", name: "Login", component: Login },
+	{ path: "/", name: "Home", component: Home },
+]
 
 const router = createRouter({
 	history: createWebHistory(),
