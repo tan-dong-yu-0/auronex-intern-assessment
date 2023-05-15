4236e28df714773ccfedd6a11c6f992a82746c83
tan-dong-yu-0 <dytan0709@gmail.com>
2023-05-15 18:46:14 +0800

    added comment on early return

--- Q1/app.py
+++ Q1/app.py
@@ -1,5 +1,6 @@
 def f(n):
     for i in range(1, n+1):
+        # prevent early return, if 15 is checked by 3 or 5, it will return either fizz or buzz instead of fizzbuzz
         if i % 3 == 0 and i % 5 == 0:
             print("fizzbuzz")
         elif i % 3 == 0:
