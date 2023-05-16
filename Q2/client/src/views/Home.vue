ebb73f860fce967d7839eb64b9880eec1f04a45c
tan-dong-yu-0 <dytan0709@gmail.com>
2023-05-16 15:12:08 +0800

    added search by product and search by category

--- Q2/client/src/views/Home.vue
+++ Q2/client/src/views/Home.vue
@@ -1,9 +1,13 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 
+// issue:
+// products array will 
+
 const products = ref([])
 const currentPage = ref(1)
-
+const searchProduct = ref('')
+const searchCategory = ref('')
 
 onMounted(async () => {
 	fetchProducts()
@@ -37,10 +41,63 @@ function previousPage() {
 	}
 }
 
+function searchProductByName() {
+	// fetch products by name
+	fetch(`https://dummyjson.com/products/search?q=${searchProduct.value}`)
+		// return the response as json
+		.then(res => res.json())
+		// not using filter because we need all the element
+		// filter will only return the element that matches the condition
+		.then(data => {
+			products.value = data.products.map(product => ({
+				name: product.title,
+				image: product.images[0],
+				price: product.price,
+				category: product.category,
+			}));
+			console.log(data)
+		});
+
+	setTimeout(() => {
+		console.log("5 seconds")
+		if (searchProduct.value === '') {
+			fetchProducts()
+		}
+	}, 3000)
+}
+
+function searchProductByCategory() {
+	fetch(`https://dummyjson.com/products/category/${searchCategory.value}`)
+		.then(res => res.json())
+		.then(data => {
+			products.value = data.products.map(product => ({
+				name: product.title,
+				image: product.images[0],
+				price: product.price,
+				category: product.category,
+			}));
+		});
+
+	setTimeout(() => {
+		console.log("5 seconds")
+		if (searchCategory.value === '') {
+			fetchProducts()
+		}
+	}, 3000)
+}
+
 </script>
 
 <template>
 	<h1>Home Page</h1>
+	<form @submit.prevent="searchProductByName">
+		<input type="text" v-model="searchProduct" placeholder="Search Product" />
+		<button type="submit">Search Product</button>
+	</form>
+	<form @submit.prevent="searchProductByCategory">
+		<input type="text" v-model="searchCategory" placeholder="Search Category" />
+		<button type="submit">Search Category</button>
+	</form>
 	<ul v-for="product in products">
 		<img :src="product.image" :alt="product.name">
 		<p>{{ product.name }}</p>
